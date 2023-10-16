babe73d87e3aafdfea601b4d266eee5496332c1e
Holly Tyler <hollystertyler@gmail.com>
2023-10-16 15:02:58 +0100

    tweaked some things

--- App.jsx
+++ App.jsx
@@ -1,8 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
-import CustomMapView from './components/MapComponent';
-import GooglePlacesInput from './components/Search';
-import UserContainer from './containers/UserContainer';
-import * as React from "react";
+import * as Location from 'expo-location';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from "./components/HomeScreen";
@@ -11,17 +7,42 @@ import MapScreen from './components/MapScreen';
 import UserProfileScreen from './components/UserProfileScreen';
 import CityProfile from './components/CityProfile';
 import CityList from './components/CityList';
-import MyTabs from './components/MainTabNavigator';
 import CheckInList from './components/CheckInList';
+import React, { useState, useEffect } from 'react';
+import MyTabs from './components/MainTabNavigator';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
     <>
     <NavigationContainer>
-    <Stack.Navigator initialRouteName="Home">
-      {/* <MyTabs/> */}
+    {/* <Stack.Navigator initialRouteName="Home"> */}
+      <MyTabs/>
       <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Here.'}}/>
       <Stack.Screen name="Destinations" component={DestinationScreen} options={{ title: 'Destinations'}}/>
       <Stack.Screen name="Map" component={MapScreen} options={{ title: 'Map'}}/>
@@ -30,7 +51,7 @@ export default function App() {
       <Stack.Screen name="City List" component={CityList} options={{ title: 'City List'}}/> 
       <Stack.Screen name="Checked-In List" component={CheckInList} options={{ title: 'Checked-In List'}}/> 
 
-      </Stack.Navigator>
+      {/* </Stack.Navigator> */}
       </NavigationContainer>
       </>
   );
